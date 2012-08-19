01dd939828244da1287df48e38b33433f4a973be
Diftraku <diftraku@derpy.me>
2012-08-19 16:01:44 +0300

    Cleaned some code and changed the repo URI

--- cloudflare/plugin.php
+++ cloudflare/plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: CloudFlare
-Plugin URI: https://github.com/Diftraku/YOURLS-CloudFlare-Plugin/
+Plugin URI: https://github.com/Diftraku/yourls_cloudflare/
 Description: Fixes incoming IPs to use the CloudFlare CF-Connecting-IP instead of CDN server IP
 Version: 1.0
 Author: Diftraku
@@ -12,9 +12,9 @@ Author URI: http://derpy.me/
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
 // Add a filter to get_IP for the real IP instead of the CDN reverse proxy
-yourls_add_filter( 'get_IP', 'cloudflare_get_IP');
-function cloudflare_get_IP($ip) {
-	if ( !empty( $_SERVER['HTTP_CF_CONNECTING_IP'] ) ) {
+yourls_add_filter( 'get_IP', 'cloudflare_get_ip');
+function cloudflare_get_ip( $ip ) {
+	if ( isset( $_SERVER['HTTP_CF_CONNECTING_IP'] ) ) {
 		$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
 	}
 	return yourls_sanitize_ip( $ip );
