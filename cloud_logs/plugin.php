08eba3b918e72f45338eee50d7853367db821af4
Toni Kaija <diftraku@derpy.me>
2014-11-29 16:41:52 +0200

    Changes to reflect on the more generic nature
    Changed the name to ReverseProxy

--- cloud_logs/plugin.php
+++ cloud_logs/plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
-Plugin Name: Cloud_logs
+Plugin Name: ReverseProxy
 Plugin URI: https://github.com/Diftraku/yourls_cloudflare/
-Description: Fixes incoming IPs to use the client IP in headers 
+Description: Fixes incoming IPs to use the client IP from reverse proxies
 Version: 2.0
 Author: Diftraku
 Author URI: http://derpy.me/
@@ -11,9 +11,9 @@ Author URI: http://derpy.me/
 // Block direct access to the plugin
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-// Add a filter to get_IP for the real IP instead of the CDN reverse proxy
-yourls_add_filter( 'get_IP', 'cloudflare_get_ip');
-function cloudflare_get_ip( $ip ) {
+// Add a filter to get_IP for the real IP instead of the reverse proxy
+yourls_add_filter( 'get_IP', 'reverseproxy_get_ip');
+function reverseproxy_get_ip( $ip ) {
 	if ( isset( $_SERVER['HTTP_CF_CONNECTING_IP'] ) ) {
 		$ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
 	} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
